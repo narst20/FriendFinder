25c3b2f12d03ed4e6518be20df0ab875ed03eb5c
narst20 <narst20@gmail.com>
2017-07-20 19:35:11 -0400

    Phase 1

--- data/friends.js
+++ data/friends.js
@@ -1,1 +1,47 @@
-ECHO is on.
+var friends = [
+  {
+    name: "Hammer",
+    occupation: "Bring the pain", 
+    photo: "http://www.qygjxz.com/images-animals/5623429.html",
+    scores: ["5", "1", "2", "3", "4", "4", "2", "5", "4", "1"]
+  },
+  {
+    name: "George",
+    occupation: "Be king",
+    photo: "http://www.qygjxz.com/lion-wallpaper/3987457.html",
+    scores: ["3", "5", "5", "2", "4", "1", "4", "3", "2", "1"]
+  },
+  {
+    name: "Ole",
+    occupation: "Puss in Boots",
+    photo: "http://www.qygjxz.com/cat-pics/4515797.html",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Dumbo",
+    occupation: "Learn to fly",
+    photo: "http://www.qygjxz.com/images-animals/4743331.html",
+    scores: ["3",  "1", "2", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Huskette",
+    occupation: "To big part of Paw Patrol",
+    photo: "http://www.qygjxz.com/cute-animal-pictures/4492223.html",
+    scores: [ "3", "2", "4", "1", "4", "2", "2", "3", "5", "5"] 
+  },
+  {
+    name: "Dorren",
+    occupation: "Find my home",
+    photo: "http://www.qygjxz.com/fish-animal-pictures/4367552.html",
+    scores: ["2", "3", "1", "3", "2", "1", "4", "5", "4", "5"]
+  },
+  {
+    name: "Hermit",
+    occupation: "To be left alone",
+    photo: "http://www.qygjxz.com/dragon-image/4261687.html",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  }
+
+];
+
+module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,1 +1,42 @@
-ECHO is on.
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+
+   app.get("/api/animals", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/animals", function(req, res) {
+
+    var bestMatch = {
+      name: "",
+      occupation: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
+
+    for (var i = 0; i < friends.length; i++) {
+
+      console.log(friends[i].name);
+      totalDifference = 0;
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = friends[i].name;
+          bestMatch.occupation = occupation[i].occupation;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
+
+  });
+
+};
